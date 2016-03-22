a8066ba568bdeb7e376455083bf4ea442ee9325c
qimuyunduan <84607842@qq.com>
2016-03-22 16:05:38 +0800

    add webView

--- XCtest/XCtest/ViewController.swift
+++ XCtest/XCtest/ViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIGestureRecognizerDelegate {
+class ViewController: UIViewController,UIGestureRecognizerDelegate,UIWebViewDelegate {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         
         
         self.title   = "标题"
+        let url      = NSURL(string: "http://www.tudou.com")
+        let request  = NSURLRequest(URL: url!)
+        webView.delegate = self
+        webView.backgroundColor = UIColor.grayColor()
+        webView.loadRequest(request)
+        
+        
         let button   = UIButton(type: .System)
         button.frame = CGRectMake(0, 0, 65, 30)
         button.setTitle("返回", forState: .Normal)
@@ -22,17 +31,30 @@ class ViewController: UIViewController,UIGestureRecognizerDelegate {
         
         let spacer = UIBarButtonItem(barButtonSystemItem:.FixedSpace, target: nil, action: nil)
         //消除左边空隙
-        spacer.width = -10
+        spacer.width = -20
         self.navigationItem.leftBarButtonItems = [spacer,leftBarButton]
         //启用滑动放回
         self.navigationController?.interactivePopGestureRecognizer?.delegate = self
-        super.viewDidLoad()
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: nil)
+        swipeGesture.delegate = self
+        self.webView.addGestureRecognizer(swipeGesture)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    //是否应该同时识别其他的手势识别器
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
     func back() {
     
         self.navigationController?.popViewControllerAnimated(true)
     
+    
+    }
+   func  webViewDidFinishLoad( webView: UIWebView) {
+    
+        print("加载完毕....")
     
     }
     override func didReceiveMemoryWarning() {
