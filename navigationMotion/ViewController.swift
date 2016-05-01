e03accfaa3bbaa22cfac118c5dbd7152ee6294f0
qimuyunduan <84607842@qq.com>
2016-05-01 10:29:30 +0800

    cannot launch

---
+++ navigationMotion/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  navigationMotion
+//
+//  Created by qimuyunduan on 16/5/1.
+//  Copyright © 2016年 qimuyunduan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController:UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
