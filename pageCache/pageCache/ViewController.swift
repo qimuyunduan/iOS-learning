83f2a9ae31a85b7054fd1bec180925c6a75f4bf3
qimuyunduan <84607842@qq.com>
2016-05-14 21:33:32 +0800

    网页缓存

---
+++ pageCache/pageCache/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pageCache
+//
+//  Created by qimuyunduan on 16/5/14.
+//  Copyright © 2016年 qimuyunduan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
