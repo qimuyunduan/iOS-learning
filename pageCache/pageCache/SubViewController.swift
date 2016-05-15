47e7b43607c67e2b4a1f3eee1129afd7c9b648ad
qimuyunduan <84607842@qq.com>
2016-05-15 09:38:13 +0800

    page Cache

---
+++ pageCache/pageCache/SubViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SubViewController.swift
+//  pageCache
+//
+//  Created by qimuyunduan on 16/5/14.
+//  Copyright © 2016年 qimuyunduan. All rights reserved.
+//
+
+import UIKit
+class SubViewController: ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
--- pageCache/pageCache/ViewController.swift
+++ pageCache/pageCache/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func loadSubView(sender: UIButton) {
+        //get subViewController form storyBoard
+        let picController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("subView")
+        
+        
+        // --1-- add subView from subViewController
+        self.view.addSubview(picController.view)
+        
+        //--2-- add subViewController
+        addChildViewController(picController)
+        picController.didMoveToParentViewController(self)
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
